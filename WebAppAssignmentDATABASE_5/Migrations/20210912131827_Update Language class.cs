5939ece2f02c18e45a38fe03fedf421c2d6a2e65
Ramya-cloud-hub <ramya118gowda@gmail.com>
2021-09-12 15:20:40 +0200

    Changes dpne in index.cshtml

---
+++ WebAppAssignmentDATABASE_5/Migrations/20210912131827_Update Language class.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppAssignmentDATABASE_5.Migrations
+{
+    public partial class UpdateLanguageclass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
